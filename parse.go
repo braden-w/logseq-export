9de1899a04951764d22c7ecfe6a0ae829590bb5d
Braden Wong <bmw02002turbo@gmail.com>
2023-02-08 21:16:02 -0500

    fix: match frontmatter and body properly

--- parse.go
+++ parse.go
@@ -1,6 +1,9 @@
 package main
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+)
 
 func parseTextAndAttributes(rawContent string) (string, map[string]string) {
 	// A set of regexes to parse the frontmatter, taking in the entire file content as a single string
@@ -8,7 +11,12 @@ func parseTextAndAttributes(rawContent string) (string, map[string]string) {
 	// result := regexp.MustCompile(`^((?:.*?::.*\n)*)\n?((?:.|\s)+)$`).FindStringSubmatch(rawContent)
 	// frontmatterArray := regexp.MustCompile(`(?m:^(.*?)::\s*(.*)$)`).FindAllStringSubmatch(result[1], -1)
 	// For Obsidian Frontmatter
-	result := regexp.MustCompile(`^---((?:.*?:.*\n)*)\n---\n?((?:.|\s)+)$`).FindStringSubmatch(rawContent)
+	result := regexp.MustCompile(`^---\n((?:.|\n)*?)\n---\n?((?:.|\s)+)$`).FindStringSubmatch(rawContent)
+
+	// Print the result
+	fmt.Println(rawContent)
+	fmt.Println(result)
+
 	frontmatterArray := regexp.MustCompile(`(?m:^(.*?):\s*(.*)$)`).FindAllStringSubmatch(result[1], -1)
 	attributes := map[string]string{}
 	for _, attrStrings := range frontmatterArray {
